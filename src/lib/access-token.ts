b60753d8ab0097dce961cb757885ef1243d090b2
attaryu <mattarannaufal@gmail.com>
2025-08-22 05:33:37 +0700

    feat: add removeAccessToken function

--- src/lib/access-token.ts
+++ src/lib/access-token.ts
@@ -1,4 +1,8 @@
-export const getAccessToken = () => localStorage.getItem('ACCESS_TOKEN');
+const KEY = 'ACCESS_TOKEN';
+
+export const getAccessToken = () => localStorage.getItem(KEY);
 
 export const storeAccessToken = (token: string) =>
-	localStorage.setItem('ACCESS_TOKEN', token);
+	localStorage.setItem(KEY, token);
+
+export const removeAccessToken = () => localStorage.removeItem(KEY);
