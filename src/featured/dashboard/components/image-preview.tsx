560154576ee2f608537c69364fff1d3cf11b17d2
attaryu <mattarannaufal@gmail.com>
2025-08-24 11:57:29 +0700

    refactor: simplify ImagePreview component by removing conditional rendering

--- src/featured/dashboard/components/image-preview.tsx
+++ src/featured/dashboard/components/image-preview.tsx
@@ -2,17 +2,15 @@ type Props = {
 	src?: string;
 };
 
-export function ImagePreview({ src }: Props) {
-	if (src) {
-		return (
-			<div className="p-2 rounded-xl border border-dashed">
-				<img
-					src={src ?? '#'}
-					alt=""
-					data-error="The image is broken :("
-					className="w-full relative h-[400px] object-contain rounded-lg before:content-[attr(data-error)] before:absolute before:inset-0 before:bg-accent before:text-foreground before:text-sm before:grid  before:place-items-center "
-				/>
-			</div>
-		);
-	}
+export function ImagePreview({ src = '#' }: Props) {
+	return (
+		<div className="p-2 rounded-xl border border-dashed">
+			<img
+				src={src}
+				alt=""
+				data-error={src === '#' ? 'Image preview' : 'The image is broken :('}
+				className="w-full relative aspect-video object-contain rounded-lg before:content-[attr(data-error)] before:absolute before:inset-0 before:bg-accent before:text-foreground before:text-sm before:grid  before:place-items-center "
+			/>
+		</div>
+	);
 }
