f061e79f38ea857aae0746211ea2c4ade9461084
attaryu <mattarannaufal@gmail.com>
2025-08-30 23:18:37 +0700

    feat: add rich text editor component
    - Created a ButtonController component for rich text editor toolbar buttons.
    - Added functionality for text formatting options (bold, italic, underline) and list management (bullet and ordered lists).
    - Enhanced the editor with text alignment options (left, justify).

---
+++ src/featured/dashboard/components/rich-text-editor/button-controller.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+
+type Props = {
+	children: React.ReactNode;
+	onClick: () => void;
+	active?: boolean;
+};
+
+export function ButtonController({ children, onClick, active }: Props) {
+	return (
+		<Button
+			onClick={onClick}
+			variant={active ? 'default' : 'ghost'}
+			className="size-10 rounded-none"
+		>
+			{children}
+		</Button>
+	);
+}
