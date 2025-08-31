953cfb9840d0ca97283cd63f5e270635743ea15a
attaryu <mattarannaufal@gmail.com>
2025-08-31 12:59:28 +0700

    feat: add `width` and `height` props to ImagePreview component

--- src/featured/dashboard/components/image-preview.tsx
+++ src/featured/dashboard/components/image-preview.tsx
@@ -1,15 +1,19 @@
 type Props = {
 	src?: string;
+	width?: number;
+	height?: number;
 };
 
-export function ImagePreview({ src = '#' }: Props) {
+export function ImagePreview({ src = '#', width = 400, height = 200 }: Props) {
 	return (
 		<div className="p-2 rounded-xl border border-dashed">
 			<img
 				src={src}
 				alt=""
 				data-error={src === '#' ? 'Image preview' : 'The image is broken :('}
-				className="w-full relative aspect-video object-contain rounded-lg before:content-[attr(data-error)] before:absolute before:inset-0 before:bg-accent before:text-foreground before:text-sm before:grid  before:place-items-center "
+				className="w-full relative object-contain rounded-lg before:content-[attr(data-error)] before:absolute before:inset-0 before:bg-accent before:text-foreground before:text-sm before:grid  before:place-items-center "
+				width={width}
+				height={height}
 			/>
 		</div>
 	);
